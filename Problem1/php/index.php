29314cebe0b2de2b89537007b256780122f90fae
mii <mii012345@outlook.jp>
2021-04-05 10:55:42 +0900

    done Problem1

--- Problem1/php/index.php
+++ Problem1/php/index.php
@@ -6,6 +6,41 @@ $obj = $_POST['pattern'];
 
 // ここに処理を記述してください。
 
-$ans = "answer";
+$decode = json_decode($obj, true);
+$arr  =$decode["obj"];
+
+var_dump($arr);
+
+$ans = "";
+$first = true;
+
+for($i = 1; $i < 31;$i++){
+    $txt = "";
+    $flag = false;
+
+    for($j = 0; $j < count($arr); $j++){
+        $num = $arr[$j]['num'];
+        $str = $arr[$j]['text'];
+        if($i % $num == 0){
+            if($flag){
+                $txt .= " ";
+            }
+            $txt .= $str;
+            $flag = true;
+        }
+    }
+    if($txt == ""){
+        $txt = $i;
+    }
+
+    if($first){
+        $ans = $txt; 
+        $first = false;
+    }else{
+        $ans .= ", " . $txt; 
+    }
+}
+
+
 echo($ans);
 ?>
